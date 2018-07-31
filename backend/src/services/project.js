12ae81a92d60348a2ffc169144f4718bf1c91635
lijun <lijun401338@qq.com>
2018-08-01 00:12:01 +0800

    format code

--- backend/src/services/project.js
+++ backend/src/services/project.js
@@ -48,14 +48,15 @@ module.exports = {
     list: async (pageNum, pageSize) => {
         const connection = db.getConnection();
         const [ projects ] = await connection.query(`SELECT * FROM ${MODEL_NAME}`);
-	const allPromise = projects.map(p => dockerService.getServicesBy({stackId: p.stackId}));
-	const projectServices = await Promise.all(allPromise);
-	return projects.map((item, index) => {
-		return {
-			...item,
-			services: projectServices[index]
-		}
- 
-	});
+        const allPromise = projects.map(p => dockerService.getServicesBy({stackId: p.stackId}));
+        const projectServices = await Promise.all(allPromise);
+
+        return projects.map((item, index) => {
+            return {
+                ...item,
+                services: projectServices[index]
+            }
+
+        });
     }
 }
