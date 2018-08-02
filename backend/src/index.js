b8e8bf6e8dd0a1928d765d8dae43cfde34a273c2
lijun <lijun401338@qq.com>
2018-08-02 19:30:21 +0800

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,14 @@ const app = express();
 const connection = require('./helper/dbconnect.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+    else  next();
+});
 connection.init();
 require('./controllers/projectTempate')(app);
 require('./controllers/project')(app);
