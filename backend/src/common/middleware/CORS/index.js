a4357c82ad72c9d84a650d65c526a459c8f71c9d
lijun <lijun401338@qq.com>
2018-08-27 18:29:50 +0800

    finish v0.9

--- backend/src/common/middleware/CORS/index.js
+++ backend/src/common/middleware/CORS/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('*', function(req, res, next) {
+router.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
